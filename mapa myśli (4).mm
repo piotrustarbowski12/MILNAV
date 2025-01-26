545b4f066d82df37ef819f531e632fc8f95dd006
piotrustarbowski12 <153120372+piotrustarbowski12@users.noreply.github.com>
2025-01-26 09:26:54 +0100

    Add files via upload

---
+++ mapa myśli (4).mm
@@ -0,0 +1,159 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1731941119366" ID="ID_1217743255" MODIFIED="1733151366855" TEXT="System MILNAV">
+<node CREATED="1731941211486" ID="ID_612637853" MODIFIED="1731941231617" POSITION="right" TEXT="Wymagania bezpiecze&#x144;stwa">
+<node CREATED="1731941261690" ID="ID_1253947392" MODIFIED="1731941414131" TEXT="Poufno&#x15b;&#x107;">
+<arrowlink DESTINATION="ID_1253947392" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1689138220" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1253947392" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1689138220" SOURCE="ID_1253947392" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1731941417093" ID="ID_618666430" MODIFIED="1733153049007" TEXT="Przesy&#x142; i przechowywanie danych (komunikacja u&#x17c;ytkownik&#xf3;w, trasy, &#x142;adunki, dane osobowe) wy&#x142;&#x105;cznie w formie szyfrowanej"/>
+<node CREATED="1731942325645" ID="ID_1957064524" MODIFIED="1731942490939" TEXT="Ukrycie nazwy celu na ekranie nawigacji po jego potwierdzeniu"/>
+<node CREATED="1731942461349" ID="ID_110983071" MODIFIED="1731942555094" TEXT="Blokowanie dost&#x119;pu do urz&#x105;dzenia po minucie nieaktywno&#x15b;ci, je&#x15b;li nawigowanie do celu nie jest w&#x142;&#x105;czone"/>
+<node CREATED="1731942562460" ID="ID_1145070171" MODIFIED="1733152761769" TEXT="Dedykowany przycisk alarmowego kasowania danych z urz&#x105;dzenia pojazdowego w przypadku niebezpiecze&#x144;stwa przechwycenia go przez przeciwnika"/>
+</node>
+<node CREATED="1731941273548" ID="ID_855685767" MODIFIED="1731941277581" TEXT="Integralno&#x15b;&#x107;">
+<node CREATED="1731941467653" ID="ID_250292094" MODIFIED="1731941547614" TEXT="Przesy&#x142; danych w TCP i potwiedzanie sum&#x105; kontroln&#x105;"/>
+</node>
+<node CREATED="1731941283114" ID="ID_413620176" MODIFIED="1731945174139" TEXT="Dost&#x119;pno&#x15b;&#x107;">
+<node CREATED="1731941550676" ID="ID_1365851217" MODIFIED="1731941679009" TEXT="Stacje robocze i serwery wyposa&#x17c;one w UPS"/>
+<node CREATED="1731941684516" ID="ID_281114326" MODIFIED="1731941707508" TEXT="Plany awaryjne na wypadek DoS"/>
+<node CREATED="1731942721348" ID="ID_1138078210" MODIFIED="1733153134258" TEXT="Kopia zapasowa centralych i regionalnych baz danych"/>
+<node CREATED="1731942765724" ID="ID_1252774750" MODIFIED="1733153110648" TEXT="Zapasowe &#x142;&#x105;cze sieciowe dla serwer&#xf3;w"/>
+<node CREATED="1731945176711" ID="ID_1482868588" MODIFIED="1731945202851" TEXT="fukcjonalno&#x15b;&#x107; natychmiastowego wydruku w warunkach terenowych marszruty"/>
+</node>
+<node CREATED="1731941294035" ID="ID_951712337" MODIFIED="1731941372355" TEXT="Autentyczno&#x15b;&#x107;">
+<node CREATED="1731941723648" ID="ID_1656661849" MODIFIED="1731941830342" TEXT="To&#x17c;samo&#x15b;&#x107; podmiotu/obiektu potwierdzana przez administratora podczas pierwszego logowania"/>
+</node>
+<node CREATED="1731941385548" ID="ID_1311483469" MODIFIED="1731942230615" TEXT="Rozliczalno&#x15b;&#x107;">
+<node CREATED="1731941865900" ID="ID_204489438" MODIFIED="1731942272255" TEXT="Przechowywanie log&#xf3;w na 3 lata wstecz"/>
+</node>
+<node CREATED="1731941397054" ID="ID_886155584" MODIFIED="1731941405702" TEXT="Niezaprzeczalno&#x15b;&#x107;">
+<node CREATED="1731942020586" ID="ID_1211462636" MODIFIED="1733153307816" TEXT="Logi zawieraj&#x105;ce ID u&#x17c;ytownika podejmuj&#x105;cego dzia&#x142;anie w systemie"/>
+</node>
+</node>
+<node CREATED="1731941260115" HGAP="52" ID="ID_803106649" MODIFIED="1731944548869" POSITION="right" TEXT="Wymagania wydajno&#x15b;ciowe" VSHIFT="-4">
+<node CREATED="1731941647254" ID="ID_325887763" MODIFIED="1731943279411" TEXT="Czas przetwarzania danych">
+<node CREATED="1731941705672" ID="ID_468871601" MODIFIED="1731943494677" TEXT="Czas potrzebny do wyznaczania trasy (&lt;15s) przy uwzgl&#x119;dnieniu:">
+<node CREATED="1731941738409" ID="ID_432820711" MODIFIED="1731941946059" TEXT="Warunk&#xf3;w infrastrukturalnych" VSHIFT="-20"/>
+<node CREATED="1731941767098" HGAP="35" ID="ID_651063000" MODIFIED="1731941951062" TEXT="Warunk&#xf3;w terenowych"/>
+<node CREATED="1731941808572" ID="ID_1370630925" MODIFIED="1731941839486" TEXT="Warunk&#xf3;w taktycznych"/>
+</node>
+<node CREATED="1731941846518" ID="ID_250780263" MODIFIED="1731943477890" TEXT="Czas reakcji na zmian&#x119; parametr&#xf3;w trasy (&lt;5s)"/>
+</node>
+<node CREATED="1731941913775" HGAP="16" ID="ID_1461172001" MODIFIED="1731943279417" TEXT="Obs&#x142;uga wielu jednostek na raz" VSHIFT="53">
+<node CREATED="1731942111630" ID="ID_1764975194" MODIFIED="1731944619891" TEXT="Skalowalno&#x15b;&#x107; obci&#x105;&#x17c;eniowa  serwerowni"/>
+<node CREATED="1731942233921" ID="ID_1772992576" MODIFIED="1731942250150" TEXT="Synchronizacja w czasie rzeczywistym"/>
+</node>
+<node CREATED="1731942297901" ID="ID_252171343" MODIFIED="1731943279419" TEXT="Zu&#x17c;ycie zasob&#xf3;w systemowych">
+<node CREATED="1731942314022" ID="ID_829608120" MODIFIED="1731944066969" TEXT="Wydajno&#x15b;&#x107; na urz&#x105;dzeniach mobilnych (maksymalnie wykorzystanie pami&#x119;ci operacyjnej: 2GB)"/>
+<node CREATED="1731942330706" ID="ID_825970550" MODIFIED="1731944748811" TEXT="Energooszcz&#x119;dno&#x15b;&#x107; (czas dzia&#x142;ania urz&#x105;dzenia mobilnego nie mniejszy ni&#x17c; 8h)"/>
+</node>
+<node CREATED="1731942491384" ID="ID_1547438439" MODIFIED="1731943279421" TEXT="Szybko&#x15b;&#x107; dzia&#x142;ania systemu">
+<node CREATED="1731942564682" ID="ID_18227161" MODIFIED="1731943586362" TEXT="Czas inicjalizacji systemu (&lt;30s)"/>
+<node CREATED="1731942576301" ID="ID_652357866" MODIFIED="1731943967874" TEXT="Czas reakcji interfejsu u&#x17c;ytkownika (&lt;1s)"/>
+</node>
+<node CREATED="1731942523404" ID="ID_1036535400" MODIFIED="1731943279424" TEXT="Obs&#x142;uga du&#x17c;ych ilo&#x15b;ci danych">
+<node CREATED="1731942716137" ID="ID_1191664420" MODIFIED="1731943602976" TEXT="Aktualizacja map (codziennie)"/>
+<node CREATED="1731942739052" ID="ID_1713907494" MODIFIED="1731942763840" TEXT="Obs&#x142;uga danych historycznych"/>
+</node>
+<node CREATED="1731942823530" ID="ID_428651268" MODIFIED="1731943279426" TEXT="Wydajno&#x15b;&#x107; w trybie offline">
+<node CREATED="1731942967902" ID="ID_638974268" MODIFIED="1731943639184" TEXT="Obliczanie tras na podstawie lokalnie przechowywanych danych (&lt;20s)"/>
+</node>
+</node>
+<node CREATED="1731940994270" ID="ID_554183624" MODIFIED="1731943927032" POSITION="left" TEXT="Wymagania funkcjonalne">
+<node CREATED="1733153684574" ID="ID_810931786" MODIFIED="1733153766403" TEXT="Dow&#xf3;dca kolumny pieszej">
+<node CREATED="1731941035675" ID="ID_303378382" MODIFIED="1733152158658" TEXT="Wprowadzanie punkt&#xf3;w pocz&#x105;tkowych i ko&#x144;cowych trasy"/>
+<node CREATED="1731941175124" ID="ID_1082956880" MODIFIED="1733152403028" TEXT="Monitorowanie i &#x15b;ledzenie tras pojazd&#xf3;w i &#x17c;o&#x142;nierzy">
+<node CREATED="1731942777651" ID="ID_1415501946" MODIFIED="1731942791896" TEXT="&#x15a;ledzenie post&#x119;pu trasy na &#x17c;ywo"/>
+<node CREATED="1731942781614" ID="ID_573466911" MODIFIED="1733152195303" TEXT="Powiadomienia push o zbli&#x17c;aj&#x105;cych si&#x119; punktach kontrolnych"/>
+<node CREATED="1731942807218" ID="ID_110640091" MODIFIED="1731942828138" TEXT="Alerty w przypadku zjechania z planowanej trasy"/>
+<node CREATED="1731942832111" ID="ID_779013" MODIFIED="1733152254200" TEXT="Opcja wprowadzania i udost&#x119;pniania innym u&#x17c;ytkownikom informacji o zagro&#x17c;eniach na trasie"/>
+<node CREATED="1733152361337" ID="ID_827874606" MODIFIED="1733152401756" TEXT="Wy&#x15b;wietlenie estymacji op&#xf3;&#x17a;nienia"/>
+</node>
+<node CREATED="1731941345028" ID="ID_442028715" MODIFIED="1731942237896" TEXT="Raportowanie i analiza danych (generowanie raport&#xf3;w po wykonaniu trasy)">
+<node CREATED="1731942241227" ID="ID_603144813" MODIFIED="1731942283631" TEXT="Czas trasy"/>
+<node CREATED="1731942248495" ID="ID_1492809523" MODIFIED="1731942271003" TEXT=" Op&#xf3;&#x17a;nienia"/>
+</node>
+<node CREATED="1731941484491" ID="ID_819422335" MODIFIED="1731941530184" TEXT="Mo&#x17c;liwo&#x15b;&#x107; dodania punkt&#xf3;w kontrolnych podczas pokonywania trasy"/>
+<node CREATED="1731941459605" ID="ID_910820836" MODIFIED="1733152601739" TEXT="Wsp&#xf3;&#x142;praca i koordynacja dzia&#x142;a&#x144;">
+<arrowlink DESTINATION="ID_910820836" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_190182479" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_910820836" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_190182479" SOURCE="ID_910820836" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1733152615282" ID="ID_747881632" MODIFIED="1733152694384" TEXT="Wy&#x15b;wietlanie informacji o incydentach zgloszonych przez innych u&#x17c;ytkownik&#xf3;w"/>
+<node CREATED="1733152636715" ID="ID_870170337" MODIFIED="1733152637971" TEXT="komunikator pomi&#x119;dzy u&#x17c;ytkownikami r&#xf3;&#x17c;nych poziom&#xf3;w poprzez np chat tekstowy/g&#x142;osowy"/>
+</node>
+</node>
+<node CREATED="1732954801508" ID="ID_402002878" MODIFIED="1733153665781" TEXT="Kierowcy">
+<node CREATED="1731941035675" ID="ID_792663480" MODIFIED="1733152158658" TEXT="Wprowadzanie punkt&#xf3;w pocz&#x105;tkowych i ko&#x144;cowych trasy"/>
+<node CREATED="1731941175124" ID="ID_559357394" MODIFIED="1733152403028" TEXT="Monitorowanie i &#x15b;ledzenie tras pojazd&#xf3;w i &#x17c;o&#x142;nierzy">
+<node CREATED="1731942777651" ID="ID_654958471" MODIFIED="1731942791896" TEXT="&#x15a;ledzenie post&#x119;pu trasy na &#x17c;ywo"/>
+<node CREATED="1731942781614" ID="ID_1472968855" MODIFIED="1733152195303" TEXT="Powiadomienia push o zbli&#x17c;aj&#x105;cych si&#x119; punktach kontrolnych"/>
+<node CREATED="1731942807218" ID="ID_574120779" MODIFIED="1731942828138" TEXT="Alerty w przypadku zjechania z planowanej trasy"/>
+<node CREATED="1731942832111" ID="ID_1407931479" MODIFIED="1733152254200" TEXT="Opcja wprowadzania i udost&#x119;pniania innym u&#x17c;ytkownikom informacji o zagro&#x17c;eniach na trasie"/>
+<node CREATED="1733152361337" ID="ID_1082424508" MODIFIED="1733152401756" TEXT="Wy&#x15b;wietlenie estymacji op&#xf3;&#x17a;nienia"/>
+</node>
+<node CREATED="1731941345028" ID="ID_1278563236" MODIFIED="1731942237896" TEXT="Raportowanie i analiza danych (generowanie raport&#xf3;w po wykonaniu trasy)">
+<node CREATED="1731942241227" ID="ID_376113050" MODIFIED="1731942283631" TEXT="Czas trasy"/>
+<node CREATED="1731942248495" ID="ID_1902692557" MODIFIED="1731942271003" TEXT=" Op&#xf3;&#x17a;nienia"/>
+<node CREATED="1731942252154" ID="ID_1037037254" MODIFIED="1731942265103" TEXT="Zu&#x17c;ycie paliwa"/>
+</node>
+<node CREATED="1731941484491" ID="ID_749247961" MODIFIED="1731941530184" TEXT="Mo&#x17c;liwo&#x15b;&#x107; dodania punkt&#xf3;w kontrolnych podczas pokonywania trasy"/>
+<node CREATED="1731941459605" ID="ID_895623169" MODIFIED="1733152601739" TEXT="Wsp&#xf3;&#x142;praca i koordynacja dzia&#x142;a&#x144;">
+<arrowlink DESTINATION="ID_895623169" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_337120995" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_895623169" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_337120995" SOURCE="ID_895623169" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1733152615282" ID="ID_1903860250" MODIFIED="1733152694384" TEXT="Wy&#x15b;wietlanie informacji o incydentach zgloszonych przez innych u&#x17c;ytkownik&#xf3;w"/>
+<node CREATED="1733152636715" ID="ID_454005923" MODIFIED="1733152637971" TEXT="komunikator pomi&#x119;dzy u&#x17c;ytkownikami r&#xf3;&#x17c;nych poziom&#xf3;w poprzez np chat tekstowy/g&#x142;osowy"/>
+</node>
+</node>
+<node CREATED="1732954815939" ID="ID_1895806266" MODIFIED="1733151565268" TEXT="Sztab/dow&#xf3;dztwo">
+<arrowlink DESTINATION="ID_1895806266" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_405630771" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1895806266" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_405630771" SOURCE="ID_1895806266" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1731941035675" ID="ID_714647275" MODIFIED="1731941104657" TEXT="Wyznaczanie punkt&#xf3;w pocz&#x105;tkowych i ko&#x144;cowych trasy "/>
+<node CREATED="1731941622348" ID="ID_1282137095" MODIFIED="1731941696908" TEXT="Zarz&#x105;dzanie danymi misji/trasy przez dow&#xf3;dztwo"/>
+<node CREATED="1731941459605" ID="ID_1625121603" MODIFIED="1733152518346" TEXT="Koordynacja dzia&#x142;a&#x144;">
+<arrowlink DESTINATION="ID_1625121603" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_19165640" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1625121603" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_19165640" SOURCE="ID_1625121603" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1731942564787" ID="ID_1134799796" MODIFIED="1733151690462" TEXT="Podgl&#x105;d statusu pojazdu i misji/trasy"/>
+<node CREATED="1731942642971" ID="ID_995735527" MODIFIED="1733151776973" TEXT="komunikator pomi&#x119;dzy u&#x17c;ytkownikami r&#xf3;&#x17c;nych poziom&#xf3;w poprzez np chat tekstowy/g&#x142;osowy"/>
+<node CREATED="1731942656937" ID="ID_70873564" MODIFIED="1733151956172" TEXT="Udostepnienie/Wsp&#xf3;&#x142;dzielenie tras i plan&#xf3;w z uprawnionym podmiotem trzecim (np odbiorc&#x105; transportu)"/>
+</node>
+<node CREATED="1731941175124" ID="ID_361595317" MODIFIED="1733151961877" TEXT="Monitorowanie i &#x15b;ledzenie tras pojazd&#xf3;w i &#x17c;o&#x142;nierzy">
+<node CREATED="1731942777651" ID="ID_1497794152" MODIFIED="1731942791896" TEXT="&#x15a;ledzenie post&#x119;pu trasy na &#x17c;ywo"/>
+<node CREATED="1731942781614" ID="ID_1163209807" MODIFIED="1731942806892" TEXT="Powiadomienia o zbli&#x17c;aj&#x105;cych si&#x119; punktach kontrolnych"/>
+<node CREATED="1731942807218" ID="ID_1236290187" MODIFIED="1731942828138" TEXT="Alerty w przypadku zjechania z planowanej trasy"/>
+<node CREATED="1731942832111" ID="ID_1566516374" MODIFIED="1731942859615" TEXT="Opcja informowania o zagro&#x17c;eniach na trasie"/>
+</node>
+<node CREATED="1731941345028" ID="ID_1589785649" MODIFIED="1731942237896" TEXT="Raportowanie i analiza danych (generowanie raport&#xf3;w po wykonaniu trasy)">
+<node CREATED="1731942241227" ID="ID_766583167" MODIFIED="1731942283631" TEXT="Czas trasy"/>
+<node CREATED="1731942248495" ID="ID_1515890903" MODIFIED="1731942271003" TEXT=" Op&#xf3;&#x17a;nienia"/>
+<node CREATED="1731942252154" ID="ID_207777763" MODIFIED="1731942265103" TEXT="Zu&#x17c;ycie paliwa"/>
+</node>
+<node CREATED="1731941819500" ID="ID_1546561909" MODIFIED="1733152011726" TEXT="Archiwum tras wraz z historia zmian"/>
+</node>
+<node CREATED="1732954832502" ID="ID_1499293597" MODIFIED="1733150761470" TEXT="Admin">
+<node CREATED="1731941531805" ID="ID_1986401887" MODIFIED="1733151264930" TEXT="Rejestracja (auoryzacja utworzenia) u&#x17c;ytkownik&#xf3;w korzystaj&#x105;cych z systemu "/>
+<node CREATED="1731941698047" ID="ID_1104797570" MODIFIED="1733151198915" TEXT="Przydzielanie uprawnie&#x144; u&#x17c;ytkownik&#xf3;w"/>
+<node CREATED="1731941819500" ID="ID_436810323" MODIFIED="1733151004246" TEXT="Baza danych zawieraj&#x105;ca informacje o przebytych trasach"/>
+</node>
+<node CREATED="1732954843866" ID="ID_1214218219" MODIFIED="1732955451679" TEXT="Logistyka">
+<node CREATED="1731941175124" ID="ID_1796415950" MODIFIED="1731941237806" TEXT="Monitorowanie i &#x15b;ledzenie tras pojazd&#xf3;w i &#x17c;o&#x142;nierzy">
+<node CREATED="1731942777651" ID="ID_196158988" MODIFIED="1731942791896" TEXT="&#x15a;ledzenie post&#x119;pu trasy na &#x17c;ywo"/>
+<node CREATED="1731942807218" ID="ID_969274876" MODIFIED="1733149842415" TEXT="Alerty o nieautoryzowanym przemieszczaniu pojazdu"/>
+</node>
+<node CREATED="1731941345028" ID="ID_1634742522" MODIFIED="1733149926334" TEXT="Raportowanie i analiza danych (generowanie raport&#xf3;w po wykonaniu trasy)">
+<node CREATED="1731942241227" ID="ID_978040968" MODIFIED="1731942283631" TEXT="Czas trasy"/>
+<node CREATED="1731942248495" ID="ID_727440275" MODIFIED="1731942271003" TEXT=" Op&#xf3;&#x17a;nienia"/>
+<node CREATED="1731942252154" ID="ID_1027895729" MODIFIED="1731942265103" TEXT="Zu&#x17c;ycie paliwa"/>
+<node CREATED="1733149941935" ID="ID_1614141543" MODIFIED="1733149954989" TEXT="Informacje o stanie technicznym pojazdu"/>
+</node>
+<node CREATED="1731941484491" ID="ID_1620113640" MODIFIED="1731941530184" TEXT="Mo&#x17c;liwo&#x15b;&#x107; dodania punkt&#xf3;w kontrolnych podczas pokonywania trasy"/>
+</node>
+<node CREATED="1733150197659" ID="ID_665130782" MODIFIED="1733150347807" TEXT="Administrator tre&#x15b;ci">
+<node CREATED="1731941970733" HGAP="27" ID="ID_760097864" MODIFIED="1733150387582" TEXT="Zarz&#x105;dzanie alertami pogodowymi i terenowymi" VSHIFT="1">
+<arrowlink DESTINATION="ID_760097864" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_228087056" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_760097864" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_228087056" SOURCE="ID_760097864" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1731942416735" ID="ID_1721145635" MODIFIED="1733150469126" TEXT="Wprowadzaie zmiennych odpowiadajacch za warunki atmosferyczne"/>
+<node CREATED="1731942419864" ID="ID_515355361" MODIFIED="1733150586523" TEXT="Wprowadzanie zmiennych odnosnie warunkow  przejeznosci terenu"/>
+</node>
+</node>
+</node>
+</node>
+</map>
